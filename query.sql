0ac5b76c156340d8681b02df2715ffab07ce098d
SrestMilan <shrestham780@gmail.com>
2023-09-05 10:13:36 +0545

    third highest

--- query.sql
+++ query.sql
@@ -1,2 +1,6 @@
 -- find nth highest salary from employee table
 SELECT DISTINCT(SALARY) AS [NTH HIGHEST SALARY] FROM Employee ORDER BY SALARY DESC;
+
+--RETRIVE 3 rd highest salary from employee table
+SELECT MIN(SALARY) FROM Employee 
+WHERE SALARY IN (SELECT TOP(3) SALARY FROM Employee ORDER BY SALARY DESC);
