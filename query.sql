73f4b17a043713921a957c046e43574002992f64
SrestMilan <shrestham780@gmail.com>
2023-09-06 08:20:15 +0545

    added few query related to nth highest

--- query.sql
+++ query.sql
@@ -1,12 +1,15 @@
 -- find nth highest salary from employee table
 SELECT DISTINCT(SALARY) AS [Number of HIGHEST SALARY] FROM Employee ORDER BY SALARY DESC;
 
---RETRIVE 3 rd highest salary from employee table
+--RETRIVE 3 rd highest salary from employee table(1st,2nd run on SQL server and 3rd applicable in MYSQL Server)
+--1
 SELECT MIN(SALARY) FROM Employee 
 WHERE SALARY IN (SELECT TOP(3) SALARY FROM Employee ORDER BY SALARY DESC);
 
+--2
 SELECT MAX(SALARY) AS [THIRD HIGHEST] FROM Employee
 WHERE SALARY <(SELECT MAX(SALARY) FROM Employee WHERE SALARY<(SELECT MAX(SALARY) FROM Employee));
 
-SELECT SALARY FROM Employee AS t1
-WHERE 3-2=(SELECT COUNT(DISTINCT SALARY) FROM Employee AS E2 WHERE E2.SALARY>t1.SALARY);
+
+--3
+SELECT SALARY FROM Employee ORDER BY SALARY DESC LIMIT 2,1;
