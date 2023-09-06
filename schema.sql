08544fecf743425443ea637bc1933b7fe2067c06
SrestMilan <shrestham780@gmail.com>
2023-09-06 07:52:05 +0545

    some changes in quer

--- schema.sql
+++ schema.sql
@@ -19,14 +19,15 @@ CREATE TABLE Employee (
 
 INSERT INTO Employee
 	( FIRST_NAME, LAST_NAME, SALARY, JOINING_DATE, DEPARTMENT,MANAGER_ID) VALUES
-		('James', 'Smith', 100000, '9/5/2023 09:00:00', 'HR', 002),
-		('Jessica', 'Kohl', 80000, '9/5/2023 09:00:00', 'Admin', 005),
-		('Alex', 'Garner', 300000, '9/5/2023 09:00:00', 'HR', 011),
-		('Pratik', 'Pandey', 500000, '9/5/2023 09:00:00', 'Admin', 020),
-		('Christine', 'Robinson', 500000, '9/5/2023 09:00:00', 'Admin', 007),
-		('Deepak', 'Gupta', 200000, '9/5/2023 09:00:00', 'Account', 015),
-		( 'Jennifer', 'Paul', 75000, '9/5/2023 09:00:00', 'Account', 012),
-		( 'Deepika', 'Sharma', 90000, '9/5/2023 09:00:00', 'Admin', 017);
+		('James', 'Smith', 100000, '2014-04-02 09:00:00', 'HR', 002),
+		('Jessica', 'Kohl', 80000, '2014-04-02 09:00:00', 'Admin', 005),
+		('Alex', 'Garner', 300000, '2014-04-02 09:00:00', 'HR', 011),
+		('Pratik', 'Pandey', 500000, '2014-04-02 09:00:00', 'Admin', 020),
+		('Christine', 'Robinson', 500000, '2014-04-02 09:00:00', 'Admin', 007),
+		('Deepak', 'Gupta', 200000, '2014-04-02 09:00:00', 'Account', 015),
+		( 'Jennifer', 'Paul', 75000, '2014-04-02 09:00:00', 'Account', 012),
+		( 'Deepika', 'Sharma', 90000, '2014-04-02 09:00:00', 'Admin', 017);
+	
 
 -- created table Salary with foreign key i.e. EMPLOYEE_REF_ID takes refrences EMPLOYEE_ID from parent table Employee--
 CREATE TABLE Salary (
