97123dca6fd8a6a62a0584407d7068a70ad79ed2
SrestMilan <shrestham780@gmail.com>
2023-09-06 08:25:36 +0545

    finding n record

--- query.sql
+++ query.sql
@@ -1,15 +1,17 @@
 -- find nth highest salary from employee table
-SELECT DISTINCT(SALARY) AS [Number of HIGHEST SALARY] FROM Employee ORDER BY SALARY DESC;
-
---RETRIVE 3 rd highest salary from employee table(1st,2nd run on SQL server and 3rd applicable in MYSQL Server)
---1
+-- 1. RETRIVE 3 rd highest salary from employee table(1.1,1.2 run on SQL server and 1.3 and 1.2 applicable in MYSQL Server)
+--1.1
 SELECT MIN(SALARY) FROM Employee 
 WHERE SALARY IN (SELECT TOP(3) SALARY FROM Employee ORDER BY SALARY DESC);
-
---2
+--1.2
 SELECT MAX(SALARY) AS [THIRD HIGHEST] FROM Employee
 WHERE SALARY <(SELECT MAX(SALARY) FROM Employee WHERE SALARY<(SELECT MAX(SALARY) FROM Employee));
-
-
---3
+--1.3
 SELECT SALARY FROM Employee ORDER BY SALARY DESC LIMIT 2,1;
+
+
+--2 Write a SQL query to find top n records?
+SELECT SALARY FROM EmployEe ORDER BY SALARY DESC;
+
+
+
